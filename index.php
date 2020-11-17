7368d1ae40523a0bd7f8d1670eebba160c944a2e
devops-999 <67201945+devops-999@users.noreply.github.com>
2020-11-17 20:11:24 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,36 +1,8 @@
 <html>
-<head>
-   <title>Тест 1</title>
-   <?php
-      require_once $_SERVER['DOCUMENT_ROOT'].'/head.php';
-   ?>
-</head>
-
 <body>
-   <div class="shapka">
-      <?php
-         require_once $_SERVER['DOCUMENT_ROOT'].'/top.php';
-      ?>
-   </div>
-   <div class="page">
-      <div class="sidebar_left">
-         <?php
-            require_once $_SERVER['DOCUMENT_ROOT'].'/sidebar1.php';
-         ?>
-      </div>
-      <div class="content">
-         ... Основная часть сайта...
-      </div>
-      <div class="sidebar_right">
-         <?php
-            require_once $_SERVER['DOCUMENT_ROOT'].'/sidebar2.php';
-         ?>
-      </div>
-      <div class="footer">
-         <?php
-            require_once $_SERVER['DOCUMENT_ROOT'].'/footer.php';
-         ?>
-      </div>
-   </div>
+
+<h1>My First Heading</h1>
+<p>My first paragraph.</p>
+
 </body>
 </html>
